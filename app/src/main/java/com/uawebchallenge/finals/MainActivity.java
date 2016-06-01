4c9d88fb9db87c83df6bb952d2d043bc3ed844a2
Roman Mazur <rmazur@stanfy.com.ua>
2016-06-01 23:01:56 +0300

    Add first app template

---
+++ app/src/main/java/com/uawebchallenge/finals/MainActivity.java
@@ -0,0 +1,20 @@
+package com.uawebchallenge.finals;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    ImageView imageView = (ImageView) findViewById(R.id.image);
+
+    // TODO: Draw the image.
+    // What image? Start with https://dev-challenge-backend.appspot.com/start
+    imageView.setImageResource(R.mipmap.ic_launcher);
+  }
+}
